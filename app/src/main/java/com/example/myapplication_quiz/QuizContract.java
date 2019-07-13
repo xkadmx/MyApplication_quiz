725ed8bf7028ef9b0ad082e1f8a871ca6c0b1c98
Zuzanna <zuzanna.rosinska@yahoo.com>
2019-07-13 22:47:28 +0200

    adding class that will work as a container for different constants

--- app/src/main/java/com/example/myapplication_quiz/QuizContract.java
+++ app/src/main/java/com/example/myapplication_quiz/QuizContract.java
@@ -1,4 +1,19 @@
 package com.example.myapplication_quiz;
 
-public final class QuizContract {  //container for different constans
+import android.provider.BaseColumns;
+
+
+public final class QuizContract implements BaseColumns {  //container for different constants
+
+    private QuizContract(){}//this way we cannot create a new object of this class
+
+
+    public static class QuestionsTable{
+        public static final String TABLE_NAME = "quiz_questions";
+        public static final String COLUMN_QUESTION = "question";
+        public static final String COLUMN_OPTION1 = "option1";
+        public static final String COLUMN_OPTION2 = "option2";
+        public static final String COLUMN_OPTION3 = "option3";
+        public static final String COLUMN_ANSWER_NR = "answer_nr";
+    }
 }
