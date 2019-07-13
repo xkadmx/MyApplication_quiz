a78b3b8d7fe4a17696b912ebc47fbbbf83365b61
Zuzanna <zuzanna.rosinska@yahoo.com>
2019-07-13 21:56:15 +0200

    changing confirm TextView to Button widget initializing startQuiz method by providing onClickListener creating intent launching QuizActivity.class after clicking the Quiz Button

--- app/src/main/java/com/example/myapplication_quiz/MainActivity.java
+++ app/src/main/java/com/example/myapplication_quiz/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.myapplication_quiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonStartQuiz = findViewById(R.id.button_start_quiz);
+        buttonStartQuiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startQuiz();
+            }
+        });
+    }
+    private void startQuiz(){
+        Intent intent = new Intent(MainActivity.this, QuizActivity.class);
+        startActivity(intent);
     }
 }
