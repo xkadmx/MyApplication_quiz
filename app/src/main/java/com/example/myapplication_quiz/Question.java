8beb9b1f59523b7628d3b5d34495a6907b9f5396
Zuzanna <zuzanna.rosinska@yahoo.com>
2019-08-12 22:45:24 +0200

    passing the difficulty String class variable to the Question class member variables and parameters as well

--- app/src/main/java/com/example/myapplication_quiz/Question.java
+++ app/src/main/java/com/example/myapplication_quiz/Question.java
@@ -14,17 +14,20 @@ public class Question implements Parcelable { // makes it possible to send Quest
     private String option1;
     private String option2;
     private String option3;
+    private String difficulty;
 
     private int answerNr;
 
     public Question(){}   //Question is an object
 
-    public Question(String question, String option1, String option2, String option3, int answerNr) {
+    public Question(String question, String option1, String option2,
+                    String option3, int answerNr, String difficulty ){
         this.question = question;
         this.option1 = option1;
         this.option2 = option2;
         this.option3 = option3;
         this.answerNr = answerNr;
+        this.difficulty = difficulty;
 
         //these are generated constructors for the constants
     }
